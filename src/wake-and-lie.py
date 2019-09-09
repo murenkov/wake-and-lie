d7582f611edacf5c98f41654a4933b32312e44e6
Murenkov <yaroslav.murenkov@gmail.com>
2019-09-10 01:49:11 +0300

    Bug fixes and new bugs

--- src/wake-and-lie.py
+++ src/wake-and-lie.py
@@ -1,17 +1,22 @@
 import re 
 from bs4 import BeautifulSoup as BSoup
-
+from pprint import pprint
 
 if __name__ == "__main__":
     TIMESTAMP = re.compile("(\d\d)\.(\d\d)\.(\d{4}) ((\d\d:\d\d):\d\d)")
-    WAKING_UP_TIME = re.compile("(Встал) (\d?\d:\d\d)") 
-    LYING_DOWN_TIME = re.compile("(Л[ё|е]г) (\d?\d:\d\d)") 
+    WAKING_UP_TIME = re.compile("(Встал) (\d?\d:\d\d)", re.M) 
+    LYING_DOWN_TIME = re.compile("(Л[ё|е]г) (\d?\d:\d\d)", re.M) 
+    WL_TIME = re.compile("(Встал|Л[ё|е]г) (\d?\d:\d\d)") 
     PARSER = 'html5lib'
 
-    input_file_name = 'messages.html'
+    input_file_name = '../messages1.html'
     table = {}
     with open(input_file_name, 'r') as doc:
-        messages = BSoup(doc.read(), PARSER).find_all(name='div', attrs={'class': 'default'})
+        messages = BSoup(
+                doc.read().replace('<br>', '\n'),
+                PARSER,
+                ).find_all(name='div', attrs={'class': 'default'})
+
         for message in messages:
             body = message.find(name='div', attrs={'class': 'body'})  
             
@@ -21,13 +26,22 @@ if __name__ == "__main__":
                 continue
             
             ts_groups = TIMESTAMP.match(timestamp.get('title')).groups()
-            timestamp = '{}-{}-{}T{}'.format(ts_groups[2], ts_groups[1], ts_groups[0], ts_groups[3])
+            timestamp = '{}-{}-{}T{}'.format(
+                    ts_groups[2],
+                    ts_groups[1],
+                    ts_groups[0],
+                    ts_groups[3],
+                    )
             text = text.text.strip()
             
-            print('timestamp: {}\ntext: {}\n'.format(timestamp, text))
+            print(f'{text}')
+            ivent = [WAKING_UP_TIME.match(text), LYING_DOWN_TIME.match(text)]
+            print(f'{ivent}')
+            if ivent[0]:
+                ivent.append(timestamp)
+            if ivent[1]:
+                ivent.append(timestamp)
 
-            up_time = WAKING_UP_TIME.match(text)
-            down_time = LYING_DOWN_TIME.match(text)
-
-            if up_time:
-                table.setdefault(timestamp, text)
+            print(f'timestamp: {ivent[2]}\ntext: {ivent[:1]}\n')
+    
+    pprint(table)
