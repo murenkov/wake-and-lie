2675ccf3c51fce752f53d1178d3b2f63a8884e19
Murenkov <yaroslav.murenkov@gmail.com>
2019-09-10 12:25:30 +0300

    Add pizdec

--- src/wake-and-lie.py
+++ src/wake-and-lie.py
@@ -3,17 +3,17 @@ from bs4 import BeautifulSoup as BSoup
 from pprint import pprint
 
 if __name__ == "__main__":
-    TIMESTAMP = re.compile("(\d\d)\.(\d\d)\.(\d{4}) ((\d\d:\d\d):\d\d)")
+    MESSAGE_TIMESTAMP = re.compile("(\d\d)\.(\d\d)\.(\d{4}) ((\d\d:\d\d):\d\d)")
+    TIMESTAMP = re.compile('((\d{4})-(\d\d)-(\d\d))T((\d\d):(\d\d))([+|-](\d\d):(\d\d))')
     WAKING_UP_TIME = re.compile("(Встал) (\d?\d:\d\d)", re.M) 
     LYING_DOWN_TIME = re.compile("(Л[ё|е]г) (\d?\d:\d\d)", re.M) 
-    WL_TIME = re.compile("(Встал|Л[ё|е]г) (\d?\d:\d\d)") 
     PARSER = 'html5lib'
 
-    input_file_name = '../messages1.html'
-    table = {}
+    input_file_name = '../../messages.html'
+    table = set([])
     with open(input_file_name, 'r') as doc:
         messages = BSoup(
-                doc.read().replace('<br>', '\n'),
+                doc.read(),
                 PARSER,
                 ).find_all(name='div', attrs={'class': 'default'})
 
@@ -25,7 +25,7 @@ if __name__ == "__main__":
             if not timestamp or not text:
                 continue
             
-            ts_groups = TIMESTAMP.match(timestamp.get('title')).groups()
+            ts_groups = MESSAGE_TIMESTAMP.match(timestamp.get('title')).groups()
             timestamp = '{}-{}-{}T{}'.format(
                     ts_groups[2],
                     ts_groups[1],
@@ -34,14 +34,20 @@ if __name__ == "__main__":
                     )
             text = text.text.strip()
             
-            print(f'{text}')
-            ivent = [WAKING_UP_TIME.match(text), LYING_DOWN_TIME.match(text)]
-            print(f'{ivent}')
+            ivent = [WAKING_UP_TIME.search(text), LYING_DOWN_TIME.search(text)]
             if ivent[0]:
+                ivent[0] = ivent[0].string()
                 ivent.append(timestamp)
             if ivent[1]:
+                ivent[1] = ivent[1].string()
                 ivent.append(timestamp)
+            for iv in ivent[:]:
+                if not iv:
+                    ivent.remove(iv)
 
-            print(f'timestamp: {ivent[2]}\ntext: {ivent[:1]}\n')
-    
+            if len(ivent) == 2:
+                table.add(tuple(ivent))
+            if len(ivent) == 4:
+                table.add((ivent[0], ivent[2]))
+                table.add((ivent[1], ivent[3]))
     pprint(table)
