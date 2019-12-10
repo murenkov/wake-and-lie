e261913cda97c23231513793cac0c810bc618ac0
Murenkov <yaroslav.murenkov@gmail.com>
2019-12-10 16:43:43 +0300

    Format with black

--- src/wake-and-lie.py
+++ src/wake-and-lie.py
@@ -7,46 +7,45 @@ from pprint import pprint
 def get_bodies(messages: list) -> list:
     bodies = []
     for message in messages:
-        body = message.find(attrs={'class': 'body'})
+        body = message.find(attrs={"class": "body"})
         if body:
             bodies.append(body)
     return bodies
 
+
 def convert_timestamp(timestamp: list) -> str:
-    return '{}-{}-{}T{}'.format(
-            timestamp[2],
-            timestamp[1],
-            timestamp[0],
-            timestamp[3],
-            )
+    return "{}-{}-{}T{}".format(timestamp[2], timestamp[1], timestamp[0], timestamp[3])
+
 
 if __name__ == "__main__":
     MESSAGE_TIMESTAMP = re.compile("(\d\d)\.(\d\d)\.(\d{4}) ((\d\d:\d\d):\d\d)")
-    TIMESTAMP = re.compile('((\d{4})-(\d\d)-(\d\d))T((\d\d):(\d\d))([+|-](\d\d):(\d\d))')
-    WAKING_UP_TIME = re.compile("(Встал) (\d?\d:\d\d)", re.M) 
-    LYING_DOWN_TIME = re.compile("(Л[ё|е]г) (\d?\d:\d\d)", re.M) 
-    PARSER = 'html5lib'
+    TIMESTAMP = re.compile(
+        "((\d{4})-(\d\d)-(\d\d))T((\d\d):(\d\d))([+|-](\d\d):(\d\d))"
+    )
+    WAKING_UP_TIME = re.compile("(Встал) (\d?\d:\d\d)", re.M)
+    LYING_DOWN_TIME = re.compile("(Л[ё|е]г) (\d?\d:\d\d)", re.M)
+    PARSER = "html5lib"
 
-    
     input_file_name = sys.argv[1]
     table = set([])
-    with open(input_file_name, 'r') as doc:
-        messages = BSoup(
-                doc.read(),
-                PARSER,
-                ).find_all(name='div', attrs={'class': 'default'})
+    with open(input_file_name, "r") as doc:
+        messages = BSoup(doc.read(), PARSER).find_all(
+            name="div", attrs={"class": "default"}
+        )
 
         for body in get_bodies(messages):
-            timestamp = body.find(attrs={'class':'date'})
+            timestamp = body.find(attrs={"class": "date"})
             if not timestamp:
                 continue
-            timestamp = convert_timestamp(MESSAGE_TIMESTAMP.match(timestamp.get('title')).groups())
-            
-            text = body.find(attrs={'class':'text'})
+            timestamp = convert_timestamp(
+                MESSAGE_TIMESTAMP.match(timestamp.get("title")).groups()
+            )
+
+            text = body.find(attrs={"class": "text"})
             if not text:
                 continue
             text = text.text.strip()
-            
+
             ivent = [WAKING_UP_TIME.search(text), LYING_DOWN_TIME.search(text)]
             if ivent[0]:
                 ivent.append(timestamp)
@@ -62,4 +61,4 @@ if __name__ == "__main__":
                 table.add((ivent[2], ivent[0].string))
                 table.add((ivent[3], ivent[1].string))
     for stamp, ivent in sorted(table, key=(lambda item: item[0])):
-        print(f'(\'{stamp}\', \'{ivent}\')')
+        print(f"('{stamp}', '{ivent}')")
